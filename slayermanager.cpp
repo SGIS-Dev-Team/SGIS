967f1bf7fa87b6fcd9ad4c8c9057547002686613
shepard_liu <1057927664@qq.com>
2021-05-02 21:56:48 +0800

    Layer View Basical Funcionality: 40%

--- slayermanager.cpp
+++ slayermanager.cpp
@@ -2,18 +2,78 @@
 
 SLayerManager::SLayerManager()
 {
-    std::list<SObject*>::const_iterator iter = mLayerList.begin();
+    //初始化图层数据模型
+    QStringList header;
+    header << tr("Visible") << tr(" ") << tr("Name");
+    mLayerModel.setHorizontalHeaderLabels(header);
 }
 
 void SLayerManager::addLayer(SObject *obj)
 {
+    if(!obj)
+        throw "Object Pointer = nullptr";
     this->mLayerList.push_back(obj);
+
+    this->mLayerModel.insertRow(0, createItem(obj));
 }
 
 void SLayerManager::addLayers(std::vector<SObject *> objVec)
 {
     for(SObject* obj : objVec)
+    {
+        if(!obj)
+            throw "Object Pointer = nullptr";
         this->mLayerList.push_back(obj);
+
+        this->mLayerModel.insertRow(0, createItem(obj));
+    }
+}
+
+void SLayerManager::replaceLayer(size_t pos, SObject* newLayer)
+{
+    *iterAt(pos) = newLayer;
+}
+
+void SLayerManager::replaceLayer(SObject *oldLayer, SObject *newLayer)
+{
+    *std::find(mLayerList.begin(), mLayerList.end(), oldLayer) = newLayer;
+}
+
+void SLayerManager::replaceLayer(std::list<SObject*>::iterator it, SObject *newLayer)
+{
+    *it = newLayer;
+}
+
+void SLayerManager::removeLayer(SObject *obj)
+{
+    if(!obj)
+        return;
+    std::list<SObject*>::iterator iter = std::find(mLayerList.begin(), mLayerList.end(), obj);
+
+    if(iter == mLayerList.end())
+        return;
+
+    removeLayer(iter);
+}
+
+void SLayerManager::removeLayer(size_t pos)
+{
+    std::list<SObject*>::iterator iter = iterAt(pos);
+
+    if(iter == mLayerList.end())
+        return;
+
+    removeLayer(iter);
+}
+
+void SLayerManager::removeLayer(std::list<SObject*>::iterator it)
+{
+    //从选中链表中删除图层
+    mSelectedLayerIterList.remove(it);
+    mLayerList.erase(it);
+
+    if(*it)
+        delete *it;
 }
 
 std::list<SObject*> & SLayerManager::getLayerList()
@@ -21,14 +81,9 @@ std::list<SObject*> & SLayerManager::getLayerList()
     return mLayerList;
 }
 
-SObject &SLayerManager::layerAt(size_t pos) const
+SObject &SLayerManager::layerAt(size_t pos)
 {
-    if(pos > mLayerList.size())
-        throw "Array Index out of bound.";
-    std::list<SObject*>::const_iterator iter = mLayerList.begin();
-    for(size_t i = 0; i < pos; ++i)
-        ++iter;
-    return *(*iter);
+    return *(*iterAt(pos));
 }
 
 const SObject *SLayerManager::clickSelect(const QPoint & pt)
@@ -56,7 +111,8 @@ const SObject *SLayerManager::clickSelect(const QPoint & pt)
                 mSelectedLayerIterList.push_back(iter.base());
                 (*iter)->setSelected(true);
             }
-
+            //发射选择改变信号
+            emit selectStateChanged();
             return *iter;
         }
     }
@@ -66,4 +122,30 @@ const SObject *SLayerManager::clickSelect(const QPoint & pt)
 void SLayerManager::clearSelection()
 {
     this->mSelectedLayerIterList.clear();
+    emit selectStateChanged();
+}
+
+std::list<SObject *>::iterator SLayerManager::iterAt(size_t pos)
+{
+    if(pos > mLayerList.size())
+        throw "Array Index out of bound.";
+    std::list<SObject*>::iterator iter = mLayerList.begin();
+    for(size_t i = 0; i < pos; ++i)
+        ++iter;
+    return iter;
+}
+
+QStandardItem *SLayerManager::createItem(SObject *obj)
+{
+    if(!obj)
+        throw "Object Pointer = nullptr";
+
+    QStandardItem* newItem = new QStandardItem;
+    newItem->setCheckable(true);
+    newItem->setDragEnabled(true);
+    newItem->setDropEnabled(true);
+
+    //newItem->set
+
+    return newItem;
 }
--- slayermanager.h
+++ slayermanager.h
@@ -3,44 +3,70 @@
 
 #include <list>
 #include <sobject.h>
+#include <QStandardItemModel>
+#include <QItemSelectionModel>
 
 //------------------------------------------
 //      SLayerManager类
 //         图层管理器
 //  该类含有一个由SObject对象指针构成的链表
 //  注意：SObject对象在类外构造时，对象资源由该类接管，
-//不要在类外释放其内存也不要再访问该对象。
+//不要在类外释放其内存。
 //
 //
 //
 //------------------------------------------
 
-class SLayerManager
+class SLayerManager: public QObject
 {
-
+    Q_OBJECT
     /*-----构造函数与析构函数-----*/
 public:
     explicit SLayerManager();
+
+    /*-----信号-----*/
+signals:
+    void selectStateChanged();
+
+    /*-----槽函数-----*/
+private slots:
 
     /*-----成员变量-----*/
 private:
     //图层链表
     std::list<SObject*> mLayerList;
-
     //选中的图层在链表中的迭代器组成的链表
     std::list<std::list<SObject*>::iterator> mSelectedLayerIterList;
+
+    //图层数据模型
+    QStandardItemModel mLayerModel;
+    //图层选中模型
+    QItemSelectionModel mSelectedLayerModel;
 
     /*-----成员函数-----*/
 
     //[访问与修改函数]
 public:
+    //添加图层在顶部
     void addLayer(SObject *obj);
     void addLayers(std::vector<SObject*> objVec);
+
+    //更换图层
+    void replaceLayer(size_t pos, SObject* newLayer);
+    void replaceLayer(SObject* oldLayer, SObject* newLayer);
+    void replaceLayer(std::list<SObject*>::iterator it, SObject* newLayer);
+
+    //删除图层
+    void removeLayer(SObject* obj);
+    void removeLayer(size_t pos);
+    void removeLayer(std::list<SObject*>::iterator it);
+
+    //搜索图层:搜索方向自顶向下
 
     //获取图层链表
     std::list<SObject *> &getLayerList();
     //按位置（从下往上数）获取图层
-    SObject &layerAt(size_t pos)const;
+    SObject &layerAt(size_t pos);
 
     //按对象类型获取图层
 
@@ -52,8 +78,10 @@ public:
 
     //[功能函数]
 private:
-
-
+    //使用位置索引链表迭代器：O(n),尽量单次使用
+    std::list<SObject *>::iterator iterAt(size_t pos);
+    //使用SObject*初始化图层数据项
+    QStandardItem *createItem(SObject* obj);
 };
 
 #endif // SLAYERMANAGER_H
